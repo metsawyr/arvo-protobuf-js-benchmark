827958cb11933ab206dff26202c5e84fb2ad65e4
Nikk Radetskiy <metsawyr@gmail.com>
2019-03-27 19:07:40 +0200

    cycles implemented

--- src/generate.js
+++ src/generate.js
@@ -1,17 +1,16 @@
 const faker = require('faker');
-const fs = require('fs');
-const path = require('path');
 
-const obj = {
-    id: faker.random.uuid(),
-    name: faker.lorem.word(),
-    children: Array(250000).fill(null).map(
-        () => ({
-            id: faker.random.uuid(),
-            name: faker.lorem.word(),
-        })
-    )
+module.exports = function (children) {
+    children = children;
+
+    return {
+        id: faker.random.uuid(),
+        name: faker.lorem.word(),
+        children: Array(children).fill(null).map(
+            () => ({
+                id: faker.random.uuid(),
+                name: faker.lorem.word(),
+            })
+        )
+    };
 };
-
-const data = JSON.stringify(obj);
-fs.writeFileSync(path.resolve(__dirname + '/data/original.json'), data);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,51 @@
-const fs = require('fs').promises;
-const path = require('path');
 const protocols = require('./protocols');
+const generate = require('./generate');
+
+const CYCLES = 20;
 
 (async () => {
-    const originalPath = path.resolve(__dirname + '/data/original.json');
-    const originalData = JSON.parse(
-        await fs.readFile(originalPath)
-    );
-    const originalStats = await fs.stat(originalPath);
+    const results = {};
 
-    console.log(`Original data size: ${originalStats.size} bytes`);
+    for (let i = 0; i < CYCLES; i++) {
+        const originalData = generate(random(10000, 250000));
+        const originalSize = JSON.stringify(originalData).length;
 
-    for (const protocol of protocols) {
-        await protocol.init();
-        const start = Date.now();
-        const serializedData = await protocol.serialize(originalData);
-        const duration = Date.now() - start;
-        const serializedPath = path.resolve(__dirname + `/data/${protocol.name}.bin`);
-        await fs.writeFile(serializedPath, serializedData);
-        const stats = await fs.stat(serializedPath);
+        for (const protocol of protocols) {
+            if (!results.hasOwnProperty(protocol.name)) {
+                results[protocol.name] = {
+                    duration: [],
+                    size: [],
+                };
+            }
+            const {duration, size} = results[protocol.name];
 
+            await protocol.init();
+            duration[i] = Date.now();
+            const serializedData = await protocol.serialize(originalData);
+            duration[i] = Date.now() - duration[i];
+            size[i] = originalSize - serializedData.length;
+        }
+    }
+
+    for (const [protocolName, benchmarks] of Object.entries(results)) {
         console.log(
-            `\n\nSerialization using ${protocol.name}`
-            + `\nDuration: ${duration} miliseconds`
-            + `\nSaved: ${originalStats.size - stats.size} bytes`
+            `\n\n${protocolName}:`
+            + `\nDuration: ${
+                Math.pow(
+                    benchmarks.duration.reduce((result, item) => result * item, 1),
+                    1 / CYCLES
+                )
+            } miliseconds`
+            + `\nSaved: ${
+                Math.pow(
+                    benchmarks.size.reduce((result, item) => result * item, 1),
+                    1 / CYCLES
+                )
+            } bytes`
         );
     }
 })();
+
+function random(min, max){
+    return Math.floor(Math.random() * ( max - min + 1) + min);
+}
